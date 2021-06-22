443b7c1b7673a34f5524d7f1890260b335c19331
Adam Cottrill <adam.cottrill@ontario.ca>
2021-06-22 18:07:58 -0400

    Update to setup 0.0.3a
    A minor bug fix to address problems in the FN125 api filters.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="fn_portal",
-    version="0.0.2b",
+    version="0.0.3a",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",  # example license
