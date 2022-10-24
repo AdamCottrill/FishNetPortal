ae5befb8fb309af04f56ff1187738c21412de5ed
Adam Cottrill <adam.cottrill@ontario.ca>
2022-10-24 14:28:47 -0400

    Compiled version 2022.10.24

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="fn_portal",
-    version="2022.9.6",
+    version="2022.10.24",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",  # example license
